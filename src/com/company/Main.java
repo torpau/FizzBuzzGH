8456f77dd4370e8a6bf91abaee8158f32ace0785
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-10 16:24:51 +0200

    creation and import of FizzBuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
-
+// Made by Torsten Pause and Filip Peric
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 1; i <= 100; i++){
+
+            if(i == 42){
+                System.out.println("Answer to the ultimate Question of life, the Universie, end everything");
+            } else if(i % 3 == 0 && i % 5 == 0 ){
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+
+        }
     }
 }
