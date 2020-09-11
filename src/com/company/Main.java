38cf8180d97e04baab5ff96d6352599bc04abba6
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-11 22:42:08 +0200

    Ultimate cut

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main{
         for (int i = 1; i <= 100; i++){
 
             if(i == 42){
-                System.out.println("Answer to the ultimate Question of life, the Universie, end everything.");
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, end Everything");
             }else if(i % 3 == 0 && i % 5 == 0 ){
                 System.out.println("FizzBuzz");
             }else if(i % 3 == 0){
