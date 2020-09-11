7d623ef77022079d90a029e31535ff047f754579
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-11 09:33:54 +0200

    final cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,19 @@
 package com.company;
 // Made by Torsten Pause and Filip Peric
-public class Main {
+public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         for (int i = 1; i <= 100; i++){
 
             if(i == 42){
-                System.out.println("Answer to the ultimate Question of life, the Universie, end everything");
-            } else if(i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("Answer to the ultimate Question of life, the Universie, end everything.");
+            }else if(i % 3 == 0 && i % 5 == 0 ){
                 System.out.println("FizzBuzz");
-            } else if(i % 3 == 0) {
+            }else if(i % 3 == 0){
                 System.out.println("Fizz");
-            } else if(i % 5 == 0) {
+            }else if(i % 5 == 0){
                 System.out.println("Buzz");
-            } else {
+            }else{
                 System.out.println(i);
             }
 
